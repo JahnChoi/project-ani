291a089c9272cd062fccc4c612d4f430326bc8b7
John Choi <john.wonjin.choi@gmail.com>
2023-06-29 00:05:24 -0700

    Updating favicon and bug when an episode number can be a double

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.json": "ad3f6c21ac94ab15c74c0360830f7fdf",
-"assets/AssetManifest.smcbin": "cb42b0a774bda83a082eb8209606d7d1",
-"assets/assets/appainter_theme.json": "e2ccfd8a7e500bd45b9b360615a03e47",
+const RESOURCES = {"assets/AssetManifest.json": "220abc36f09e94b41136da775d34a55b",
+"assets/AssetManifest.smcbin": "0707bc1741ed47da9fb464872b34dca8",
+"assets/assets/appainter_theme.json": "24832a965190485577e16c9956502740",
+"assets/assets/appainter_theme_old_backup.json": "e2ccfd8a7e500bd45b9b360615a03e47",
+"assets/assets/launcher_icon.png": "df257c42fdeaae6fe82283854c8284ee",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/NOTICES": "39773e096bd8d15602e7f37fa35e6de2",
+"assets/NOTICES": "2c7951d15e99b78c46e6d00e4d7b5667",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
@@ -19,17 +21,17 @@ const RESOURCES = {"assets/AssetManifest.json": "ad3f6c21ac94ab15c74c0360830f7fd
 "canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
 "canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
 "canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "31129c0261354f2f4c4e9e90ead0ccd1",
 "flutter.js": "6b515e434cea20006b3ef1726d2c8894",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "48d42a75df562be793bd8688155a290b",
-"/": "48d42a75df562be793bd8688155a290b",
-"main.dart.js": "ae260772439c3dfcdc629c2e761820ae",
-"manifest.json": "032e122884d1408f3cd3d9cea26be353",
-"version.json": "24adfb1f66b0a46e5a3504d6075ae196"};
+"icons/Icon-192.png": "fd3e5d8fb1f3a84685a89a38c560870a",
+"icons/Icon-512.png": "91e98ff96699f6718a97e6d4c65fdf83",
+"icons/Icon-maskable-192.png": "fd3e5d8fb1f3a84685a89a38c560870a",
+"icons/Icon-maskable-512.png": "91e98ff96699f6718a97e6d4c65fdf83",
+"index.html": "5935da27315240c5340b8e9e63b69b39",
+"/": "5935da27315240c5340b8e9e63b69b39",
+"main.dart.js": "3ad90fff3f34523f92c564be05a2967e",
+"manifest.json": "e63a958a27d0b9a5d7f50566f4e1681f",
+"version.json": "6426c6245b8a686f4619e01e8cf231a2"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
